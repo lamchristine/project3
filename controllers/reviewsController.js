985f88e74c6483c59dc7d209ec27e6f0387691ad
lamchristine <christine.lam.06@gmail.com>
2016-05-29 01:59:31 -0700

    fixed routing

--- controllers/reviewsController.js
+++ controllers/reviewsController.js
@@ -10,6 +10,9 @@ function index(req, res) {
   Review
     .find({})
     .populate('user')
+    .populate('item')
+    .populate('place')
+    .populate('category')
     .exec(function(err, reviews){
       if (err || !reviews || !reviews.length) {
         return res.status(404).send({message: 'Reviews not found.'});
@@ -20,16 +23,22 @@ function index(req, res) {
 
 function create(req, res){
   db.Item.findById(req.params.itemId, function (err, foundItem) {
-    console.log("*****", req.params)
-    // saving the new review in reviews and assigning reference to user id and category id
-  var new_review = new Review(req.body);
-     new_review.item = req.params.itemId;
-     new_review.user = req.user_id;
-    //  new_review.category = req.params.categoryId;
-     new_review.save(function(err, new_review){
-
-       res.send(new_review);
-     });
+    // saving the new review in reviews and assigning reference to user id and item id
+    var new_review = new Review(req.body);
+    new_review.item = req.params.itemId;
+    new_review.user = req.user_id;
+    new_review.save(function(err, new_review){
+      res.send(new_review);
+    });
+    //pushing new_review_id to array of reviews in category
+    var cat_id = foundItem.category;
+    db.Category.findById(cat_id, function (err, foundCat) {
+      foundCat.reviews.push(new_review);
+      foundCat.save();
+    });
+    //pushing new_review_id to array of reviews in item
+    foundItem.reviews.push(new_review);
+    foundItem.save();
   });
 }
 
@@ -63,6 +72,9 @@ function show(req, res){
   Review
     .findById(req.params.id)
     .populate('user')
+    .populate('item')
+    .populate('place')
+    .populate('category')
     .exec(function(err, found_review){
       if (err || !found_review) {
         return res.status(404).send({message: 'Review not found.'});
