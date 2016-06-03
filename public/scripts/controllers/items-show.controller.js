b127ef3ca3beef19fc42866a397f5a7401b56495
lamchristine <christine.lam.06@gmail.com>
2016-06-02 22:55:52 -0400

    fixed item map to show marker

--- public/scripts/controllers/items-show.controller.js
+++ public/scripts/controllers/items-show.controller.js
@@ -20,38 +20,29 @@ function ItemsShowController ($location, $http, $routeParams) {
 
       // show marker on map
       L.mapbox.accessToken = 'pk.eyJ1IjoiY2xhbTA2IiwiYSI6ImNpb294d25xMzAwMXJ1eG0zdXdtaGtqMmwifQ.baGJ8eoCmGvgnMptSrTGyw';
-
-      var map = L.mapbox.map('map', 'mapbox.streets')
-          .setView([37.730098, -122.469546], 16);
-
-          L.marker([-122.413385, 37.75264], {
-              icon: L.mapbox.marker.icon({
-                  'marker-size': 'large',
-                  'marker-symbol': 'bus',
-                  'marker-color': '#fa0'
-              })
-          }).addTo(map);
-
-      //     L.mapbox.featureLayer({
-      //     // this feature is in the GeoJSON format: see geojson.org
-      //     // for the full specification
-      //     type: 'Feature',
-      //     geometry: {
-      //         type: 'Point',
-      //         // coordinates here are in longitude, latitude order because
-      //         // x, y is the standard for GeoJSON and many formats
-      //         coordinates: [-122.413385, 37.75264]
-      //     },
-      //     properties: {
-      //         title: 'Peregrine Espresso',
-      //         description: '1718 14th St NW, Washington, DC',
-      //         // one can customize markers by adding simplestyle properties
-      //         // https://www.mapbox.com/guides/an-open-platform/#simplestyle
-      //         'marker-size': 'large',
-      //         'marker-color': '#BE9A6B',
-      //         'marker-symbol': 'marker'
-      //     }
-      // }).addTo(map);
+    var map = L.mapbox.map('map', 'mapbox.streets')
+        .setView([coord_arr[1], coord_arr[0]], 16);
+// [-122.4194, 37.7749]
+    L.mapbox.featureLayer({
+        // this feature is in the GeoJSON format: see geojson.org
+        // for the full specification
+        type: 'Feature',
+        geometry: {
+            type: 'Point',
+            // coordinates here are in longitude, latitude order because
+            // x, y is the standard for GeoJSON and many formats
+            coordinates: coord_arr
+        },
+        properties: {
+            title: 'Peregrine Espresso',
+            description: '1718 14th St NW, Washington, DC',
+            // one can customize markers by adding simplestyle properties
+            // https://www.mapbox.com/guides/an-open-platform/#simplestyle
+            'marker-size': 'large',
+            'marker-color': '#BE9A6B',
+            'marker-symbol': 'marker'
+        }
+    }).addTo(map);
     }
 
     function onGetError(response){
