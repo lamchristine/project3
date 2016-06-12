518d5bb271f889c86fb0716e44ff841069ecd413
lamchristine <christine.lam.06@gmail.com>
2016-06-12 16:57:26 -0400

    reverting

--- models/index.js
+++ models/index.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
-mongoose.connect( 'mongodb://localhost/Project3App' ||
-                  process.env.MONGOLAB_URI ||
-                  process.env.MONGOHQ_URL );
+mongoose.connect(   process.env.MONGODB_URI ||
+                  process.env.MONGOHQ_URL ||
+                  'mongodb://localhost/Project3App');
 
 
 module.exports = {
